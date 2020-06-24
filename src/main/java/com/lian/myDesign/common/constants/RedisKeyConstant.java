d305d88537bad3116afbe661f498c0d7f9877a0b
liantengda <1666893831@qq.com>
2020-06-24 09:48:38 +0800

    我的设计模式项目初始搭建

---
+++ src/main/java/com/lian/myDesign/common/constants/RedisKeyConstant.java
@@ -0,0 +1,13 @@
+package com.lian.myDesign.common.constants;
+
+/**
+ * redis常量key
+ */
+public class RedisKeyConstant {
+	/**
+	 * token key={user_userid}
+	 */
+	public static final String TOKEN = "user_";
+
+
+}
---
+++ src/main/java/com/lian/myDesign/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,56 @@
+package com.lian.myDesign.service.impl;
+
+import com.lian.myDesign.common.enums.MyBusinessExceptionEnum;
+import com.lian.myDesign.mapper.PermissionMapper;
+import com.lian.myDesign.model.Permission;
+import com.lian.myDesign.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Ted
+ * @version 1.0
+ * @date 2020/6/2 21:23
+ */
+@Service
+public class PermissionServiceImpl implements PermissionService {
+
+    @Autowired
+    PermissionMapper permissionMapper;
+
+    @Override
+    public Permission add(Permission permission) {
+        permission.setCreateTime(System.currentTimeMillis());
+        Permission add = permissionMapper.add(permission);
+        Permission byId = permissionMapper.findById(permission.getId());
+        return byId;
+    }
+
+    @Override
+    public Permission update(Permission permission) {
+        Permission update = permissionMapper.update(permission);
+        return update;
+    }
+
+    @Override
+    public List<Permission> list() {
+        List<Permission> list = permissionMapper.list();
+        return list;
+    }
+
+    @Override
+    public Permission delete(Long id) {
+        Permission byId = permissionMapper.findById(id);
+        MyBusinessExceptionEnum.USER_ALREADY_DELETED.assertNotNull(byId);
+        Permission delete = permissionMapper.delete(id);
+        return delete;
+    }
+
+    @Override
+    public Permission findById(Long id) {
+        Permission byId = permissionMapper.findById(id);
+        return byId;
+    }
+}
